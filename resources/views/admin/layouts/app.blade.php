396fff287c832ba4b129d9238468263c639ab7d3
Sohedul Islam <sohedul.islam@gmail.com>
2022-08-15 13:26:54 +0600

    Upload and set up dashboard template

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<!-- Mirrored from dreamguys.co.in/demo/doccure/admin/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 30 Nov 2019 04:12:20 GMT -->
+<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>Doccure - Dashboard</title>
+		
+		<!-- Favicon -->
+        <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+		
+		<!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="{{asset ('admin/assets/css/bootstrap.min.css')}}">
+		
+		<!-- Fontawesome CSS -->
+        <link rel="stylesheet" href="{{asset ('admin/assets/css/font-awesome.min.css')}}">
+		
+		<!-- Feathericon CSS -->
+        <link rel="stylesheet" href="{{asset ('admin/assets/css/feathericon.min.css')}}">
+		
+		<link rel="stylesheet" href="{{asset ('admin/assets/plugins/morris/morris.css')}}">
+		
+		<!-- Main CSS -->
+        <link rel="stylesheet" href="{{asset ('admin/assets/css/style.css')}}">
+		
+		<!--[if lt IE 9]>
+			<script src="assets/js/html5shiv.min.js"></script>
+			<script src="assets/js/respond.min.js"></script>
+		<![endif]-->
+    </head>
+    <body>
+	
+		<!-- Main Wrapper -->
+        <div class="main-wrapper">
+		
+		@include('admin.layouts.header')
+
+		@include('admin.layouts.sidebar')
+
+		
+			
+			
+			
+			<!-- Page Wrapper -->
+            <div class="page-wrapper">
+			
+                <div class="content container-fluid">
+					
+					<!-- Page Header -->
+					<div class="page-header">
+						<div class="row">
+							<div class="col-sm-12">
+								<h3 class="page-title">Welcome Admin!</h3>
+								<ul class="breadcrumb">
+									<li class="breadcrumb-item active">Dashboard</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /Page Header -->
+
+					@section('main-section')
+
+					@show
+					
+					
+				</div>			
+			</div>
+			<!-- /Page Wrapper -->
+		
+        </div>
+		<!-- /Main Wrapper -->
+		
+		<!-- jQuery -->
+        <script src="{{asset ('admin/assets/js/jquery-3.2.1.min.js')}}"></script>
+		
+		<!-- Bootstrap Core JS -->
+        <script src="{{asset ('admin/assets/js/popper.min.js')}}"></script>
+        <script src="{{asset ('admin/assets/js/bootstrap.min.js')}}"></script>
+		
+		<!-- Slimscroll JS -->
+        <script src="{{asset ('admin/assets/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
+		
+		<script src="{{asset('admin/assets/plugins/raphael/raphael.min.js')}}"></script>    
+		<script src="{{asset ('admin/assets/plugins/morris/morris.min.js')}}"></script>  
+		<script src="{{asset ('admin/assets/js/chart.morris.js')}}"></script>
+		
+		<!-- Custom JS -->
+		<script  src="{{asset ('admin/assets/js/script.js')}}"></script>
+		
+    </body>
+
+<!-- Mirrored from dreamguys.co.in/demo/doccure/admin/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 30 Nov 2019 04:12:34 GMT -->
+</html>
---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,70 @@
+<!-- Sidebar -->
+<div class="sidebar" id="sidebar">
+    <div class="sidebar-inner slimscroll">
+        <div id="sidebar-menu" class="sidebar-menu">
+            <ul>
+                <li class="menu-title"> 
+                    <span>Main</span>
+                </li>
+                <li> 
+                    <a href="#"><i class="fe fe-home"></i> <span>Dashboard</span></a>
+                </li>
+                <li> 
+                    <a href="#"><i class="fe fe-home"></i> <span>Slider</span></a>
+                </li>
+                <li> 
+                    <a href="#"><i class="fe fe-home"></i> <span>Testimonial</span></a>
+                </li>
+                <li> 
+                    <a href="#"><i class="fe fe-home"></i> <span>Our clients</span></a>
+                </li>
+                <li class="submenu">
+                    <a href="#"><i class="fe fe-document"></i> <span> Portfolio</span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a href="invoice-report.html">Portfolio</a></li>
+                        <li><a href="invoice-report.html">Category</a></li>
+                        <li><a href="invoice-report.html">Tag</a></li>
+                    </ul>
+                
+                    </li>
+                <li> 
+                    <a href="#"><i class="fe fe-home"></i> <span>Our Team</span></a>
+                </li>
+
+            
+                
+                <li class="submenu">
+                    <a href="#"><i class="fe fe-document"></i> <span> Posts</span> <span class="menu-arrow"></span></a>
+                    <ul style="display: none;">
+                        <li><a href="invoice-report.html">All Posts</a></li>
+                        <li><a href="invoice-report.html">Category</a></li>
+                        <li><a href="invoice-report.html">Tags</a></li>
+                    </ul>
+                
+                    </li>
+
+                    <li class="menu-title"> 
+                        <span>Admin Options</span>
+                    </li>
+
+                    <li class="submenu">
+                        <a href="#"><i class="fe fe-document"></i> <span> Admin Users</span> <span class="menu-arrow"></span></a>
+                        <ul style="display: none;">
+                            <li><a href="invoice-report.html">Users</a></li>
+                            <li><a href="invoice-report.html">Role</a></li>
+                            <li><a href="invoice-report.html">Permission</a></li>
+                        </ul>
+                    
+                        </li>
+
+                        <li> 
+                            <a href="#"><i class="fe fe-home"></i> <span>Theme Options</span></a>
+                        </li>
+                        <li> 
+                            <a href="#"><i class="fe fe-home"></i> <span>Settings</span></a>
+                        </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<!-- /Sidebar -->
